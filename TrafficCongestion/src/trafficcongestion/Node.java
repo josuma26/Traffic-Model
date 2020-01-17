33c38ec086bcaa549741c2edfbf4694e6232eedd
DESKTOP-VS0892C\jsula <j.sulaimanm@icloud.com>
2020-01-17 15:11:13 -0500

    Adding graph theory

--- TrafficCongestion/src/trafficcongestion/Node.java
+++ TrafficCongestion/src/trafficcongestion/Node.java
@@ -5,10 +5,26 @@
  */
 package trafficcongestion;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author jsula
  */
 public class Node {
+    ArrayList<Number[]> connections;
+    String name;
+    Integer index;
     
+    public Node(String names,int index){
+        this.name = name;
+        this.index = index;
+        
+    }
+    
+    public Node(String name,int index,ArrayList<Number[]> connections){
+        this.name = name;
+        this.connections = connections;
+        this.index = index;
+    }
 }
