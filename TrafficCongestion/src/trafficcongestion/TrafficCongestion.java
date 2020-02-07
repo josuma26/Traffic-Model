454fe05f30a43247842b09cfe114aa682a3387ee
DESKTOP-VS0892C\jsula <j.sulaimanm@icloud.com>
2020-02-07 09:02:48 -0500

    Added and modified Car class

--- TrafficCongestion/src/trafficcongestion/TrafficCongestion.java
+++ TrafficCongestion/src/trafficcongestion/TrafficCongestion.java
@@ -16,6 +16,13 @@ public class TrafficCongestion {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Car c = new Car(200,200,30,10);
+        c.SetAcceleration(0);
+        c.SetSpeed(1);
+        c.setDirection(0);
+        for(int i = 0;i<10;i++){
+            c.update(1);
+        }
     }
     
 }
